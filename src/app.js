8e3adef71418cdb542d17b09cc4deeb64baab74a
Christian Fleury <chfleurysiq@gmail.com>
2021-09-16 08:09:57 -0300

    adicionando arquitetura

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv/config');
+
+const express = require('express');
+ 
+const routes = require('./routes');
+
+// import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+   }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+}
+
+module.exports = new App().server;

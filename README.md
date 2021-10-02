a02fdc607b21081b66dff097e3c11110791ab16f
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-10-01 22:42:16 -0300

    reorganiza estrutura do projeto

--- README.md
+++
@@ -1,1 +0,0 @@
-# exch-book-api
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-
-const routes = new Router();
-
-routes.get('/', (req, res) => res.json({ message: 'Olá mundo' }));
-
-module.exports = routes;

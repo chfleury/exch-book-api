44a231a48074ed2cb8dc7c7bebb8815cbfa6a907
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-10-06 19:30:32 -0300

    create Offer model

---
+++ src/app/models/Offer.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+
+class Book extends Sequelize.Model {
+    static init(sequelize) {
+        super.init(
+            {
+                is_accepted: Sequelize.BOOLEAN,
+            }, {
+                sequelize
+            }
+        )
+
+        return this
+    }
+
+    static associate(models) {
+        this.belongsTo(models.User, { foreignKey: 'user_from_id', as: 'user_from' });
+        this.belongsTo(models.User, { foreignKey: 'user_to_id', as: 'user_to' });
+
+        this.belongsTo(models.Book, { foreignKey: 'book_from_id', as: 'book_from' });
+        this.belongsTo(models.Book, { foreignKey: 'book_to_id', as: 'book_to' });
+    }
+
+}
+
+module.exports = Book

3ca5f4e2e1c9a0a0620872d33eaed26c586cfc5f
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-10-06 16:50:18 -0300

    create offers migration

--- src/app/database/migrations/20211006173634-create-book-image.js
+++ src/app/database/migrations/20211006173634-create-book-image.js
@@ -1,15 +1,59 @@
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('books', 'image_id', {
-      type: Sequelize.INTEGER,
-      references: { model: 'files', key: 'id' },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL',
-      allowNull: true,
-    });
-  },
+'use strict';
 
-  down: (queryInterface) => {
-    return queryInterface.removeColumn('books', 'image_id');
-  },
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('offers', 
+        {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        user_from_id: {
+          type: Sequelize.INTEGER,
+          references: { model: 'users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        user_to_id: {
+          type: Sequelize.INTEGER,
+          references: { model: 'users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        book_from_id: {
+          type: Sequelize.INTEGER,
+          references: { model: 'books', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        book_to_id: {
+          type: Sequelize.INTEGER,
+          references: { model: 'books', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        is_accepted: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+    });
+},
+
+down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('offers');
+    }
 };

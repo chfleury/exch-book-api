4805fe57c5acc6bec8bbcfb1a292b26e845996fc
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-10-06 16:41:08 -0300

    create offers controller

---
+++ src/app/controllers/OffersController.js
@@ -0,0 +1,24 @@
+class OffersController {
+    async store(req, res) {
+        
+    }
+
+    async index(req, res) {
+        
+    }
+
+    async indexOne(req, res) {
+        
+    }
+
+    async update(req, res) {
+        
+    }
+
+    async delete(req, res) {
+
+    }
+
+}
+
+module.exports = new UserController()

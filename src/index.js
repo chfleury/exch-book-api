8b5adcfad9d2ef629fc140e75dc7be798b57b1ff
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-10-06 19:36:08 -0300

    config files

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
 app.use(routes)
 
 app.listen(3333)

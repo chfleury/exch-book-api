6f051a993adf4a337e94c11b784fe6d6a69711ca
Christian Fleury <chfleurysiq@gmail.com>
2021-10-07 15:41:05 -0300

    fix typo

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   dialectOptions: {
       ssl: true
   },
+  ssl: { rejectUnauthorized: false },
   host: 'ec2-54-204-148-110.compute-1.amazonaws.com',
   username: 'ianvopnsmcdyms',
   password: 'dcd1790c38bb9553f6c4c6c324f684081a04afdb5f501c5f6d92d2c24709c417',

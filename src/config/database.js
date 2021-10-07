3a4bb90dee4028fd87460315a2f26f704109688f
Christian Fleury <chfleurysiq@gmail.com>
2021-10-07 15:43:38 -0300

    fix typo

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,9 @@ module.exports = {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-      ssl: true
+      ssl: true,
+      rejectUnauthorized: false 
   },
-  ssl: { rejectUnauthorized: false },
   host: 'ec2-54-204-148-110.compute-1.amazonaws.com',
   username: 'ianvopnsmcdyms',
   password: 'dcd1790c38bb9553f6c4c6c324f684081a04afdb5f501c5f6d92d2c24709c417',

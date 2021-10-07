6ff88211bde43af5b26325f214598c9644b75a29
Christian Fleury <chfleurysiq@gmail.com>
2021-10-07 15:30:56 -0300

    fix typo

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: "postgres",
-  host: 'ec2-54-204-148-110.compute-1.amazonaws.com',
-  username: 'ianvopnsmcdyms',
-  password: 'dcd1790c38bb9553f6c4c6c324f684081a04afdb5f501c5f6d92d2c24709c417',
-  database: 'dfkc43tl7esiuf',
-  uri: 'postgres://ianvopnsmcdyms:dcd1790c38bb9553f6c4c6c324f684081a04afdb5f501c5f6d92d2c24709c417@ec2-54-204-148-110.compute-1.amazonaws.com:5432/dfkc43tl7esiuf',
-  url: 'postgres://ianvopnsmcdyms:dcd1790c38bb9553f6c4c6c324f684081a04afdb5f501c5f6d92d2c24709c417@ec2-54-204-148-110.compute-1.amazonaws.com:5432/dfkc43tl7esiuf',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
